aa076acf0b82e7396b0b9140cb991ffae1906bd7
abbcyhn <abbcyhn@gmail.com>
2021-01-28 20:23:00 +0400

    Create helloasm.s

---
+++ helloasm.s
@@ -0,0 +1,31 @@
+# PURPOSE:  Simple program that exits and 
+#           returns a status code back to the Linux kernel
+
+# INPUT:    none
+
+# OUTPUT:   returns a status code
+#           This can be viewed by typing
+#                   echo $?
+#           after running the program
+
+# VARIABLES:
+# %eax holds the system call number
+# %ebx holds the return status
+#
+
+.section .data
+
+.section .text
+.globl _start
+_start:
+
+movl $1, %eax       # this is the 
+                    # linux kernel command number (system call) 
+                    # for exiting a program
+
+movl $25, %ebx      # this is the 
+                    # status number 
+                    # we will return to the operating system.
+
+int $0x80           # this wakes up the kernel to run
+                    # the exit command
