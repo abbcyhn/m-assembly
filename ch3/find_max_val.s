7b7a6802683661ad1415a4c284f85ed6685da2d6
abbcyhn <abbcyhn@gmail.com>
2021-02-05 16:35:37 +0400

    Update comments in find_max_val.s

--- ch3/find_max_val.s
+++ ch3/find_max_val.s
@@ -3,49 +3,39 @@
 # VARIABLES:
 #   %edi    -   index of the current value
 #   %eax    -   current value
-#   %ebx    -   biggest value
+#   %ebx    -   maximum value
 
 # NOTES:    0 means end of the list 
 #                   and must be replaced at the end of the list
 
 .section .data
     list:
-        .long 3, 1, 2, -5, 0
+        .long 3, 1, 2, -5, 255
 
 
 .section .text
     .globl _start
-        _start:
-            # first index must be zero
-            movl $0, %edi
+        _start:                                     # start of the program
 
-            # first value in the list is the current value
-            movl list(, %edi, 4), %eax
+            movl $0, %edi                           # set index zero
+            movl list(, %edi, 4), %eax              # set current value as first value in the list
 
-            # also, first value in the list is the biggest value
-            movl %eax, %ebx
+            movl %eax, %ebx                         # set current value as maximum value
 
             _loop:
-                # when we reach at the end of the list, we must break loop
-                cmpl $0, %eax
+                cmpl $255, %eax                     # jump end of the program, if current value (%eax=255) 
                 je end
 
-                # increment current index
-                incl %edi
+                incl %edi                           # increment current index
+                movl list(, %edi, 4), %eax          # read next value from the list
 
-                # read next value from the list
-                movl list(, %edi, 4), %eax
-
-                # well, if current value (eax) <= minimum value (ebx), then repeat loop
-                cmpl %ebx, %eax
+                cmpl %ebx, %eax                     # loop again, if current value (eax) <= maximum value (ebx)
                 jle _loop
 
-                # otherwise, set current value as minimum value, then repeat loop
-                movl %eax, %ebx
-                jmp _loop
+                movl %eax, %ebx                     # otherwise, set current value as maximum value
+                jmp _loop                           # loop again
 
 
-        # end of the program
-        end:
-            movl $1, %eax
-            int $0x80
+        end:                                        # end of the program
+            movl $1, %eax                           # system call number (sys_exit)
+            int $0x80                               # call kernel
