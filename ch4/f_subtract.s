6ce400e76f59267785999d690dd55fec7f829b4d
abbcyhn <abbcyhn@gmail.com>
2021-03-01 11:15:24 +0400

    Update f_subtract.s

--- ch4/f_subtract.s
+++ ch4/f_subtract.s
@@ -1,37 +1,49 @@
-.section .data
+# PURPOSE:  This program finds difference between two numbers using function
+.data
+    num1:
+        .long 8
+    
+    num2:
+        .long 5
 
-.section .text
-    .globl _start
-        _start:
+.text
+    _start:
+        pushl num2
+        pushl num1
+        call f_subtract
 
-            movl $20, %eax
-            movl $80, %ebx
+        movl %eax, %ebx
 
-            pushl %ebx
-            pushl %eax
+    end:
+        movl $1, %eax
+        int $0x80
 
-            call subtract
-
-            movl %eax, %ebx
-
-
-        end:
-            movl $1, %eax
-            int $0x80
-
-
-    .type subtract, @function
-        subtract:
+# PURPOSE:  Find difference between two numbers
+#
+# INPUT:    
+#           num1 - first argument
+#           num2 - second argument
+# OUTPUT:
+#           num2 - num1
+#
+# VARIABLES:
+#           %ebx - holds num1
+#           %ecx - holds num2
+#
+    .type f_subtract, @function
+    f_subtract:
+        f_subtract_start:
             pushl %ebp
             movl %esp, %ebp
 
-            movl 8(%ebp), %eax
-            movl 12(%ebp), %ebx
+        f_subtract_body:
+            movl 8(%ebp), %ebx          # ebx = num1
+            movl 12(%ebp), %ecx         # ecx = num2
+            subl %ebx, %ecx             # ecx = ecx - ebx (num2 - num1)
 
-            subl %eax, %ebx
+            movl %ecx, %eax             # eax = ecx (eax is return value)
 
-            movl %ebx, %eax
-
+        f_subtract_end:
             movl %ebp, %esp
             popl %ebp
             ret
