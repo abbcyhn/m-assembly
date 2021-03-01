7b4ab6cfb2e6c78ab48216e61d95fa49011ad36f
abbcyhn <abbcyhn@gmail.com>
2021-03-01 11:55:47 +0400

    Update f_power.s

--- ch4/f_power.s
+++ ch4/f_power.s
@@ -1,69 +1,66 @@
-# PURPOSE:  This program will compute the value of basenumber^power
+# PURPOSE: This program will computer: number^power
 
-.section .data
-    basenumber:
-        .long 2
+.data
+    number:
+        .long 5
     
     power:
         .long 3
 
-.section .text
-    .globl _start
-        _start:
-            pushl power                             # push second argument - the power
-            pushl basenumber                        # push first argument - the base number
-            call f_power                            # call the function
+.text
+    _start:
+        pushl power
+        pushl number
+        call f_power
 
-            addl $8, %esp                           # move the stack pointer back
+        movl %eax, %ebx
 
-            movl %eax, %ebx                         # save the result is in %ebx
-
-        end:                                        # end of the program
-            movl $1, %eax                           # system call number (sys_exit)
-            int $0x80                               # call kernel
+    end:
+        movl $1, %eax
+        int $0x80
 
 
-# PURPOSE:  This function is used to compute
-#           the value of a number raised to a power.
+# PURPOSE:  Find number^power
 #
 # INPUT:
-#           First argument - the base number
-#           Second argument - the power
+#           number - first argument
+#           power - second argument
 #
-# OUTPUT:   Will give the result as a return value
-#
-# NOTES:    The power must be 1 or greater
-#
+# OUTPUT:
+#           number^power
 # VARIABLES:
-#           %eax        - for temporary storage
-#           %ebx        - holds the base number
-#           %ecx        - holds the power
-#           -4(%ebp)    - for local storage, holds the current result.
-
-.type power, @function
+#           eax     - temp storage
+#           ebx     - holds number
+#           ecx     - holds power
+#           -4(ebp) - local variable. holds current result
+#
+    .type f_power, @function
     f_power:
-        pushl %ebp                                  # save old base pointer
-        movl %esp, %ebp                             # make stack pointer the base pointer
-        subl $4, %esp                               # get room for our local storage
+        f_power_start:
+            pushl %ebp
+            movl %esp, %ebp
 
-        movl 8(%ebp), %ebx                          # put base number in %ebx
-        movl 12(%ebp), %ecx                         # put power in %ecx
+        f_power_body:
+            movl 8(%ebp), %ebx          # ebx = number
+            movl 12(%ebp), %ecx         # ecx = power
 
-        movl %ebx, -4(%ebp)                         # store current result
+            subl $4, %esp               # get room for local storage
+            movl %ebx, -4(%ebp)         # save current result in local storage
 
-    f_power_loop:
-        cmpl $1, %ecx                               # if the power is 1, we are done
-        je f_power_end
-        
-        movl -4(%ebp), %eax                         # move the current result into %eax
-        imull %ebx, %eax                            # multiply the current result by the base number
-        movl %eax, -4(%ebp)                         # store the current result
+            f_power_loop:
+                cmpl $1, %ecx           # if power = 1
+                je f_power_end          # jump end of function
 
-        decl %ecx                                   # decrease the power
-        jmp f_power_loop                            # run for the next power
+                movl -4(%ebp), %eax     # save current result in temp storage
+                imull %ebx, %eax        # current result = current result * number
+                movl %eax, -4(%ebp)     # save current result in local storage
 
-    f_power_end:
-        movl -4(%ebp), %eax                         # return value goes in %eax
-        movl %ebp, %esp                             # restore the stack pointer
-        popl %ebp                                   # restore the base pointer
-        ret                                         # return
+                decl %ecx               # decrement power
+                jmp f_power_loop        # loop again
+
+            movl -4(%ebp), %eax         # set result in return value
+
+        f_power_end:
+            movl %ebp, %esp
+            popl %ebp
+            ret
