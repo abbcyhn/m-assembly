4b807356e133427cdafec5ec87b2a44afa825224
abbcyhn <abbcyhn@gmail.com>
2021-03-02 15:31:56 +0400

    Update f_strlen.s

--- ch4/f_strlen.s
+++ ch4/f_strlen.s
@@ -1,31 +1,62 @@
 # PURPOSE:  This program will compute length of string
 #
-# NOTE:
-#           Last character of string is 0
-#
 .data
     mystring:
         .ascii "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-
 .text
     _start:
-        movl $mystring, %ebx
+        pushl $mystring
+        call f_strlen
 
-        movl $0, %ecx
-        movb (%ebx,%ecx,1), %bh
-
-        loop:
-            cmpb $0, %bh
-            je loop_end
-
-            incl %eax
-            incl %ecx
-            movb (%ebx,%ecx,1), %bh
-            jmp loop
-
-        loop_end:
-            movl %eax, %ebx
+        movl %eax, %ebx
 
     end:
         movl $1, %eax
         int $0x80
+
+
+# PURPOSE:  Find length of given string
+#
+# INPUT:
+#           string pointer - first argument
+#
+# OUTPUT:
+#           length of string
+#
+# VARIABLES:
+#           %bh     - holds current character
+#           %eax    - holds string pointer
+#           %ecx    - holds length
+#           %edi    - holds index of current character
+#
+# NOTE:
+#           Last character of string is 0
+#
+.type f_strlen, @function
+f_strlen:
+    f_strlen_start:
+        pushl %ebp
+        movl %esp, %ebp
+
+    f_strlen_body:
+        movl 8(%ebp), %eax                  # eax = string pointer
+
+        movl $0, %edi                       # set current index(edi) = 0
+        movb (%eax,%ecx,1), %bh             # set current character(bh)
+
+            f_strlen_loop:
+                cmpb $0, %bh                # if current character(bh) is 0
+                je f_strlen_loop_end        # jump end of loop
+
+                incl %ecx                   # otherwise, increment length(ecx) 
+                incl %edi                   # increment current index(edi)
+                movb (%eax,%edi,1), %bh     # read next character(bh)
+                jmp f_strlen_loop           # loop again
+
+            f_strlen_loop_end:              # end of loop
+                movl %ecx, %eax             # set length to return value (eax = ecx)
+
+    f_strlen_end:
+        movl %ebp, %esp
+        popl %ebp
+        ret
